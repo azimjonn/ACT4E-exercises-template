332d0ce9cc558763342999dc8042e7fe000a49bb
me <azimjon.ibn.mehmonali@gmail.com>
2024-09-01 23:16:10 +0000

    set product

---
+++ src/act4e_solutions/my_finite_set_product.py
@@ -0,0 +1,55 @@
+from typing import Sequence, cast, Collection, Iterator, List, TypeVar
+import act4e_interfaces as I
+
+from .my_finite_sets import MyFiniteSet
+from .sets_properties import SolFiniteSetProperties
+
+C = TypeVar("C")
+
+class MyFiniteSetProduct(I.FiniteSetProduct[C, List[C]]):
+    _components: List[I.FiniteSet[C]]
+    def __init__(self, components: Sequence[I.FiniteSet[C]]):
+        self._components = list(components)
+
+    def components(self) -> Sequence[I.FiniteSet[C]]:
+        return self._components
+    
+    def contains(self, x: List[C]) -> bool:
+        if not isinstance(x, List):
+            return False
+        for elem, comp in zip(x, self._components):
+            if not comp.contains(elem):
+                return False
+        
+        return True
+
+    def size(self) -> int:
+        ans = 1
+        for c in self._components:
+            ans *= c.size()
+        return ans
+    
+    def elements(self) -> Iterator[List[C]]:
+        result = [[]]
+
+        for comp in self._components:
+            itr = comp.elements()
+            temp_result = []
+
+            for item in result:
+                for elem in itr:
+                    temp_result.append(item + [elem])
+            result = temp_result
+        
+        for product in result:
+            yield result
+    
+    def pack(self, args: Sequence[C]) -> List[C]:
+        return list(args)
+    def unpack(self, args: List[C]) -> Sequence[C]:
+        return args
+
+    def load(self, h: I.IOHelper, o: I.ConcreteRepr) -> List[C]:
+        return cast(List[C], o)
+    def save(self, h: I.IOHelper, x: List[C]) -> I.ConcreteRepr:
+        return cast(I.ConcreteRepr, x)
--- src/act4e_solutions/sets_product.py
+++ src/act4e_solutions/sets_product.py
@@ -2,10 +2,12 @@ from typing import Any, Sequence, TypeVar
 
 import act4e_interfaces as I
 
+from .my_finite_set_product import MyFiniteSetProduct
+
 X = TypeVar("X")
 
 
 class SolFiniteMakeSetProduct(I.FiniteMakeSetProduct):
 
     def product(self, components: Sequence[I.FiniteSet[X]]) -> I.FiniteSetProduct[X, Any]:
-        raise NotImplementedError()  # implement here
+        return MyFiniteSetProduct(components)
