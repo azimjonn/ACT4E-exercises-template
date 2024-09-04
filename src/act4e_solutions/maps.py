6c228cf3357658ff1b61f5b24ba0ebf5492854fd
me <azimjon.ibn.mehmonali@gmail.com>
2024-09-04 01:57:24 +0000

    finite map operations

--- src/act4e_solutions/maps.py
+++ src/act4e_solutions/maps.py
@@ -2,6 +2,8 @@ from typing import TypeVar
 
 import act4e_interfaces as I
 
+from .my_finite_map import MyFiniteMap
+
 A = TypeVar("A")
 B = TypeVar("B")
 C = TypeVar("C")
@@ -9,8 +11,12 @@ C = TypeVar("C")
 
 class SolFiniteMapOperations(I.FiniteMapOperations):
 
-    def identity(self, s: I.Setoid[A]) -> I.Mapping[A, A]:
-        raise NotImplementedError()
+    def identity(self, s: I.FiniteSet[A]) -> I.Mapping[A, A]:
+        mapping = [(key, key) for key in s.elements()]
+
+        return MyFiniteMap(s, s, mapping)
 
     def compose(self, f: I.FiniteMap[A, B], g: I.FiniteMap[B, C]) -> I.FiniteMap[A, C]:
-        raise NotImplementedError()
+        mapping = [(key, g(f(key))) for key in f.source().elements()]
+
+        return MyFiniteMap(f.source(), g.target(), mapping)
