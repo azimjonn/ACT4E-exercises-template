bb2d8075df2ebde82f45f177ab776bebffdf0506
me <azimjon.ibn.mehmonali@gmail.com>
2024-10-09 14:19:38 +0000

    monotone maps

--- src/act4e_solutions/posets_map.py
+++ src/act4e_solutions/posets_map.py
@@ -6,10 +6,17 @@ A = TypeVar("A")
 B = TypeVar("B")
 X = TypeVar("X")
 
+from .posets_bounds import SolFinitePosetConstructionOpposite
 
 class SolFiniteMonotoneMapProperties(I.FiniteMonotoneMapProperties):
     def is_monotone(self, p1: I.FinitePoset[A], p2: I.FinitePoset[B], m: I.FiniteMap[A, B]) -> bool:
-        raise NotImplementedError()
+        for x in p1.carrier().elements():
+            for y in p1.carrier().elements():
+                if p1.holds(x, y):
+                    if not p2.holds(m(x), m(y)):
+                        return False
+        
+        return True
 
     def is_antitone(self, p1: I.FinitePoset[A], p2: I.FinitePoset[B], m: I.FiniteMap[A, B]) -> bool:
-        raise NotImplementedError()
+        return self.is_monotone(SolFinitePosetConstructionOpposite().opposite(p1), p2, m)
